4e57bc78b52def3d754580ad750d6038fd7b7065
Vishal6283dk <vishal25goyal25@gmail.com>
2025-08-15 18:59:46 +0530

    Interacting Backend with Frontend Successfully

--- backend/database.py
+++ backend/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine, Column, Integer, String, Text
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 
-DATABASE_URL = "sqlite:///./agent.db"
+DATABASE_URL = "sqlite:///./backend/agent.db"
 
 engine = create_engine(DATABASE_URL, connect_args= {"check_same_thread": False})
 sessionLocal = sessionmaker(autoflush=False, autocommit=False, bind=engine)
