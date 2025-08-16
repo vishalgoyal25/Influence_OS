fb23e041c0804b52970945df19c324a2fe0c6ced
Vishal6283dk <vishal25goyal25@gmail.com>
2025-08-16 22:58:39 +0530

    Introducing LLM- Mistral-7B for Better Post Generation

--- backend/aiAgent.py
+++ backend/aiAgent.py
@@ -1,19 +1,35 @@
-from transformers import pipeline
+# from transformers import pipeline
 from backend.database import SessionLocal, GeneratedLinkedinPost
 
-print("Loading AI model... Please wait.")
-generator = pipeline("text-generation", model="distilgpt2")  # Smaller & faster model
+# print("Loading AI model... Please wait.")
+# generator = pipeline("text-generation", model="distilgpt2")  # Smaller & faster model
 
+import requests
 
 def Generate_Linkedin_Post(prompt:str, max_length: int = 200):
 
-    outputs= generator(prompt, max_new_tokens = max_length, num_return_sequences= 1,
-                       truncation=True,  pad_token_id= generator.model.config.eos_token_id,
-                       temperature=0.7, top_p=0.9,
-                       repetition_penalty=1.2, do_sample=True)
+    # outputs= generator(prompt, max_new_tokens = max_length, num_return_sequences= 1,
+    #                    truncation=True,  pad_token_id= generator.model.config.eos_token_id,
+    #                    temperature=0.7, top_p=0.9,
+    #                    repetition_penalty=1.2, do_sample=True)
 
-    post_content = outputs[0]['generated_text']
+    # post_content = outputs[0]['generated_text']
 
+    url = "http://localhost:11434/api/generate"
+    payload = {
+        "model": "mistral:7b",
+        "prompt": prompt,
+        "max_tokens": max_length,
+        "stream": False
+    }
+
+    response = requests.post(url, json=payload)
+    response.raise_for_status()
+    result = response.json()
+
+    post_content = result.get("response", "")
+
+    # Save to DB
     db= SessionLocal()
     newPost = GeneratedLinkedinPost(content= post_content)
     db.add(newPost)
@@ -22,8 +38,6 @@ def Generate_Linkedin_Post(prompt:str, max_length: int = 200):
     db.close()
 
     return post_content
-
-
 
 
 # Optional standalone test mode-->>
